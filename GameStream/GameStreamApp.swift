7ef4f9d7a0bf5c701f300bb926af911b620dbd40
Nicolas Posa <nicoposa57@gmail.com>
2024-09-01 21:23:36 -0300

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Nicolas Posa on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
