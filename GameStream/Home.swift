0400d2cb41f0524925dfa8eb80c752cb3691fd33
Nicolas Posa <nicoposa57@gmail.com>
2024-09-17 10:52:11 -0300

    home screen

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -8,32 +8,54 @@
 import SwiftUI
 
 struct Home: View {
+    @State var selectedTab:Int = 2
     var body: some View {
-        TabView(){
+        TabView(selection: $selectedTab ){
             Text("Pantalla Perfil").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "person")
                     Text("Profile")
-                }
+                }.tag(0)
             Text("Pantalla Games").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "gamecontroller")
                     Text("Games")
-                }
-            Text("Pantalla Home").font(.system(size: 30, weight: .bold, design: .rounded))
+                }.tag(1)
+            HomeScreen()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
-                }
+                }.tag(2)
             Text("Pantalla Favorites").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "heart")
                     Text("Favorites")
-                }
+                }.tag(3)
         }
-        
+        .accentColor(.white)
+    }
+    init(){
+            
+            UITabBar.appearance().backgroundColor = UIColor(Color("colorTab"))
+//            UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+//            UITabBar.appearance().isTranslucent = true
+            
+        }
+
+}
+
+
+struct HomeScreen: View {
+    var body: some View {
+        ZStack{
+            Color("Marine").ignoresSafeArea()
+            VStack {
+                Text("Hola")
+            }.padding(.horizontal, 18)
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
     }
 }
+
 
 #Preview {
     Home()
