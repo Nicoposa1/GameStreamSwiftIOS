53121e1944b522eb526bb9a0977619a2acaae8bd
Nicolas Posa <nicoposa57@gmail.com>
2024-09-22 02:27:16 -0300

    video player

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -1,17 +1,10 @@
-//
-//  Home.swift
-//  GameStream
-//
-//  Created by Nicolas Posa on 15/09/2024.
-//
-
 import SwiftUI
 import AVKit
 
 struct Home: View {
-    @State var selectedTab:Int = 2
+    @State var selectedTab: Int = 2
     var body: some View {
-        TabView(selection: $selectedTab ){
+        TabView(selection: $selectedTab) {
             Text("Pantalla Perfil").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "person")
@@ -35,88 +28,94 @@ struct Home: View {
         }
         .accentColor(.white)
     }
-    init(){
-            
-            UITabBar.appearance().backgroundColor = UIColor(Color("colorTab"))
-//            UITabBar.appearance().unselectedItemTintColor = UIColor.gray
-//            UITabBar.appearance().isTranslucent = true
-            
-        }
 
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color("colorTab"))
+    }
 }
-
 
 struct HomeScreen: View {
     @State var searchText = ""
     var body: some View {
-        ZStack{
-            Color("Marine").ignoresSafeArea()
-            VStack {
-                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.horizontal, 11)
-                HStack{
-                    Button(action: search, label: {
-                        Image(systemName: "magnifyingglass").foregroundStyle(searchText.isEmpty ? Color(.yellow) : Color("Dark-Cian"))
-                    })
-                    ZStack(alignment: .leading){
-                        if searchText.isEmpty{
-                            Text("Buscar un video").foregroundStyle(Color(red: 174/255, green: 177/255, blue: 185/255))
+        NavigationStack {
+            ZStack {
+                Color("Marine").ignoresSafeArea()
+                VStack {
+                    Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.horizontal, 11)
+                    HStack {
+                        Button(action: search, label: {
+                            Image(systemName: "magnifyingglass").foregroundStyle(searchText.isEmpty ? Color(.yellow) : Color("Dark-Cian"))
+                        })
+                        ZStack(alignment: .leading) {
+                            if searchText.isEmpty {
+                                Text("Buscar un video").foregroundStyle(Color(red: 174/255, green: 177/255, blue: 185/255))
+                            }
+                            TextField("", text: $searchText).foregroundStyle(.white)
                         }
-                        TextField("", text: $searchText).foregroundStyle(.white)
+                    }.padding([.top, .leading, .bottom], 11)
+                        .background(Color("Blue-Gray"))
+                        .clipShape(Capsule())
+                    ScrollView(showsIndicators: false) {
+                        SubModuleHome()
                     }
-                }.padding([.top, .leading, .bottom], 11)
-                    .background(Color("Blue-Gray"))
-                    .clipShape(Capsule())
-                ScrollView(showsIndicators: false) {
-                    SubModuleHome()
-                }
-            }.padding(.horizontal, 18)
-        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
+                }.padding(.horizontal, 18)
+            }
+            .navigationBarHidden(true)
+        }
     }
-    
-    func search () {
-        print("El usuario esta buscando \(searchText)")
+
+    func search() {
+        print("El usuario está buscando \(searchText)")
     }
 }
 
 struct SubModuleHome: View {
-        @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
-        @State var isPlayerActive = false
-        let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
-        
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    @State var isPlayerActive = false
+    let urlVideos: [String] = [
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"
+    ]
+    
     var body: some View {
-        VStack{
+        VStack {
             Text("THE MOST POPULAR")
                 .font(.title3)
                 .foregroundStyle(.white)
                 .bold()
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                 .padding(.top)
-            ZStack{
+            
+            ZStack {
                 Button(action: {
                     url = urlVideos[0]
                     print("URL: \(url)")
                     isPlayerActive = true
-                    
                 }, label: {
-                    VStack(spacing: 0){
+                    VStack(spacing: 0) {
                         Image("The Witcher 3").resizable().scaledToFill()
                         Text("The Witcher 3").frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                             .background(Color("Blue-Gray"))
                     }
                 })
-                
                 Image(systemName: "play.circle.fill")
                     .resizable()
                     .foregroundStyle(.white)
                     .frame(width: 42, height: 42)
-            }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .center)
+            }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
                 .padding(.vertical)
+                .navigationDestination(isPresented: $isPlayerActive) {
+                VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+                    .frame(width: 400, height: 300)
+            }
         }
-        
-        NavigationLink(destination: VideoPlayer(player: AVPlayer(url: URL(string: url)!)), label: <#T##() -> View#>)
     }
 }
-
 
 #Preview {
     Home()
