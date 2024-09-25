02380e59d267101428b4842eb589435faf6b8b80
Nicolas Posa <nicoposa57@gmail.com>
2024-09-25 00:49:41 -0300

    mvvm

---
+++ GameStream/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  GameStream
+//
+//  Created by Nicolas Posa on 25/09/2024.
+//
+
+import Foundation
---
+++ GameStream/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  GameStream
+//
+//  Created by Nicolas Posa on 25/09/2024.
+//
+
+import Foundation
