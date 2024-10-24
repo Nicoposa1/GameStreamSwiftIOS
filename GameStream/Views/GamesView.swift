943186fb57baa57280a5e48472ad566997c15b76
Nicolas Posa <nicoposa57@gmail.com>
2024-10-24 10:29:54 -0300

    show items

--- GameStream/Views/GamesView.swift
+++ GameStream/Views/GamesView.swift
@@ -1,21 +1,10 @@
-//
-//  GamesView.swift
-//  RickAndMortyApp
-//
-//  Created by Nicolas Posa on 23/09/2024.
-//
-
 import SwiftUI
 
 struct GamesView: View {
     
     @ObservedObject var allCharacters = ViewModel()
     @State var characterviewIsActive: Bool = false
-    @State var url:String = ""
-    @State var title:String = ""
-    @State var episode:Int = 0
-    @State var image:String = ""
-    @State var characters: [Character] = []
+    @State var selectedCharacter: Character? = nil
     
     let formatterGrid = [
         GridItem(.flexible()),
@@ -23,31 +12,103 @@ struct GamesView: View {
     ]
     
     var body: some View {
-        
-        
-        ZStack{
-            Color("Marine")
-            VStack{
-                
-                Text("Games")
+        ZStack {
+            Color("Marine").ignoresSafeArea()
+            
+            VStack {
+                Text("Rick and Morty Characters")
                     .font(.title2)
                     .fontWeight(.bold)
                     .foregroundStyle(.white)
                     .padding(.top, 10)
                 
-            }.padding(.horizontal, 6)
-        }
-        
-        
-        Text("Hello, World!")
-        .onAppear {
-                self.allCharacters.fetchData()
+                ScrollView {
+                    LazyVGrid(columns: formatterGrid, spacing: 20) {
+                        ForEach(allCharacters.characters, id: \.id) { character in
+                            VStack {
+                                AsyncImage(url: URL(string: character.image)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                        .clipShape(Circle())
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                
+                                Text(character.name)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                
+                                Text(character.status)
+                                    .foregroundColor(.gray)
+                                    .font(.subheadline)
+                            }
+                            .onTapGesture {
+                                selectedCharacter = character
+                                characterviewIsActive = true
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.top, 10)
             }
-        
-        VStack {
-            
+            .padding(.horizontal, 6)
         }
- 
+        .onAppear {
+            self.allCharacters.fetchData()
+        }
+        .fullScreenCover(isPresented: $characterviewIsActive) {
+            if let character = selectedCharacter {
+                CharacterDetailView(character: character)
+            }
+        }
+    }
+}
+
+struct CharacterDetailView: View {
+    var character: Character
+    
+    var body: some View {
+        ZStack {
+            Color("Marine").ignoresSafeArea()
+            
+            VStack {
+                AsyncImage(url: URL(string: character.image)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                Text(character.name)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+                
+                Text("Status: \(character.status)")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                
+                Text("Species: \(character.species)")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                
+                Text("Origin: \(character.origin.name)")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
